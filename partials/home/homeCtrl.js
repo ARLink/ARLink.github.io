34fe19df35266c862f5042284ecdbdeab4459771
ARLink <riveralongoria.a@gmail.com>
2015-06-04 13:10:21 -0500

    Update homeCtrl.js

--- partials/home/homeCtrl.js
+++ partials/home/homeCtrl.js
@@ -1,12 +1,6 @@
 'use strict';
 
 angular.module('portfolio', [])
-//.config(['$routeProvider', function($routeProvider) {
-//	$routeProvider.when('/home', {
-//		templateUrl: 'partials/home/home.html',
-//		controller: 'HomeCtrl'
-//	});
-//}])
 .controller('HomeCtrl', ['$scope', function($scope) {
 	
 }]);
