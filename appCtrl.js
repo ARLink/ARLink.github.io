27083fc1734bf126dad809457c2077ebe6ee7c3c
ARLink <riveralongoria.a@gmail.com>
2015-06-04 12:12:52 -0500

    Update appCtrl.js

--- appCtrl.js
+++ appCtrl.js
@@ -2,17 +2,6 @@
 
 var app = angular.module('portfolio', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider){
-	$routeProvider.when('/home', {
-		templateUrl: 'partials/home/home.html',
-		controller: 'homeCtrl'
-	});
-	$routeProvider.when('/contact', {
-		templateUrl: 'partials/contact/contact.html',
-		controller: 'contactCtrl'
-	});
-	$routeProvider.otherwise({redirectTo: '/home'});
-}])
 
 .controller('appCtrl', ['$scope', function($scope) {
 	//Variables and Objects
