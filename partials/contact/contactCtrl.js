262c33be035f078f7f2c963830d5f8761f2156dd
ARLink <riveralongoria.a@gmail.com>
2015-06-04 13:10:40 -0500

    Update contactCtrl.js

--- partials/contact/contactCtrl.js
+++ partials/contact/contactCtrl.js
@@ -1,12 +1,6 @@
 'use strict';
 
 angular.module('portfolio', [])
-//.config(['$routeProvider', function($routeProvider) {
-//	$routeProvider.when('/contact', {
-//		templateUrl: 'partials/contact/contact.html',
-//		controller: 'ContactCtrl'
-//	});
-//}])
 .controller('ContactCtrl', ['$scope', function($scope) {
 
 }]);
