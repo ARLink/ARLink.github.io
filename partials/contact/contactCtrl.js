f513a9f4e8954e79d348886b40cc85bd2b50f82f
ARLink <riveralongoria.a@gmail.com>
2015-06-04 11:31:26 -0500

    Moved '$scope' in contact

--- partials/contact/contactCtrl.js
+++ partials/contact/contactCtrl.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('portfolio', ['$scope'])
+angular.module('portfolio', [])
 //.config(['$routeProvider', function($routeProvider) {
 //	$routeProvider.when('/contact', {
 //		templateUrl: 'partials/contact/contact.html',
 //		controller: 'ContactCtrl'
 //	});
 //}])
-.controller('ContactCtrl', [function() {
+.controller('ContactCtrl', ['$scope', function($scope) {
 
 }]);
