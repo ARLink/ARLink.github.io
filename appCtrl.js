ccf564a12810ffdf77c103810c1d9b5ada988dfc
ARLink <riveralongoria.a@gmail.com>
2015-06-04 12:16:24 -0500

    Update appCtrl.js

--- appCtrl.js
+++ appCtrl.js
@@ -14,6 +14,29 @@ var app = angular.module('portfolio', ['ngRoute'])
 	$routeProvider.otherwise({redirectTo: '/home'});
 }])
 
-.controller('appCtrl', ['$scope', function($scope) {
-	
+.controller('appCtrl', function($scope) {
+	//Variables and Objects
+	$scope.navMode = 'full';	
+	//Helper Functions
+	var isMobile = function(){
+		if($(window).width() < 765){
+			return true;
+		}else{
+			return false;
+		};
+	};
+	//Init
+	if(isMobile()){
+		$scope.navMode = 'mobile';
+	}
+	$(window).resize(function(){
+		$scope.$apply(function(){
+			if(isMobile()){
+				$scope.navMode = 'mobile';
+			}else{
+				$scope.navMode = 'full';
+			};
+			console.log("navMode: ", $scope.navMode);
+		});
+	});	
 }]);
