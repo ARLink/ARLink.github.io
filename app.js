f147e8fc71e08097608b308fb7f75acb253e00fe
Agustin Rivera-Longoria <riveralongoria.a@gmail.com>
2015-06-03 20:07:17 -0500

    Helper Functions
    Ali's navMode logic added

--- app.js
+++ app.js
@@ -4,4 +4,32 @@ angular.module('portfolio', ['ngRoute','portfolio.home','portfolio.contact'
 ])
 .config(['$routeProvider', function($routeProvider){
 	$routeProvider.otherwise({redirectTo: '/home'});
+}])
+.controller('appCtrl', [function($scope) {
+	//Variables and Objects
+	$scope.navMode = 'full';
+	
+	//Helper Functions
+	var isMobile = function(){
+		if($(window).width() < 765){
+			return true;
+		}else{
+			return false;
+		};
+	};
+	
+	//Init
+	if(isMobile()){
+		$scope.navMode = 'mobile';
+	}
+	
+	$(window).resize(function(){
+		$scope.$apply(function(){
+			if(isMobile()){
+				$scope.navMode = 'mobile';
+			}else{
+				$scope.navMode = 'full';
+			};
+		});
+	});
 }]);
