71dc016d3a59ab07fbc9f9becccde0588cb1d1c4
ARLink <riveralongoria.a@gmail.com>
2015-06-04 11:20:18 -0500

    Update homeCtrl.js
    moving routeProvider to main app

--- partials/home/homeCtrl.js
+++ partials/home/homeCtrl.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('portfolio.home', ['ngRoute'])
-.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/home', {
-		templateUrl: 'partials/home/home.html',
-		controller: 'HomeCtrl'
-	});
-}])
+//.config(['$routeProvider', function($routeProvider) {
+//	$routeProvider.when('/home', {
+//		templateUrl: 'partials/home/home.html',
+//		controller: 'HomeCtrl'
+//	});
+//}])
 .controller('HomeCtrl', [function() {
 	
 }]);
