d6acf324e7997a793e7c94b0899b66fa3bfb87ba
ARLink <riveralongoria.a@gmail.com>
2015-06-04 12:15:20 -0500

    Update appCtrl.js

--- appCtrl.js
+++ appCtrl.js
@@ -2,30 +2,18 @@
 
 var app = angular.module('portfolio', ['ngRoute'])
 
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/home', {
+		templateUrl: 'partials/home/home.html',
+		controller: 'homeCtrl'
+	});
+	$routeProvider.when('/contact', {
+		templateUrl: 'partials/contact/contact.html',
+		controller: 'contactCtrl'
+	});
+	$routeProvider.otherwise({redirectTo: '/home'});
+}])
 
 .controller('appCtrl', ['$scope', function($scope) {
-	//Variables and Objects
-	$scope.navMode = 'full';	
-	//Helper Functions
-	var isMobile = function(){
-		if($(window).width() < 765){
-			return true;
-		}else{
-			return false;
-		};
-	};
-	//Init
-	if(isMobile()){
-		$scope.navMode = 'mobile';
-	}
-	$(window).resize(function(){
-		$scope.$apply(function(){
-			if(isMobile()){
-				$scope.navMode = 'mobile';
-			}else{
-				$scope.navMode = 'full';
-			};
-			console.log("navMode: ", $scope.navMode);
-		});
-	});
+	
 }]);
